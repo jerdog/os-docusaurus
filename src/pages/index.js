0c346a584f48ab8d0da8369b8243f740b309cefb
Jeremy Meiss <jeremy.meiss@gmail.com>
2024-11-01 06:39:05 -0500

    edit: remove hero button

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,13 +16,13 @@ function HomepageHeader() {
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
+        {/* <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/intro">
             Get Started
           </Link>
-        </div>
+        </div> */}
       </div>
     </header>
   );
