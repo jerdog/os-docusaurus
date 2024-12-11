55f3474e6e3acb160ca21fb822f9afb118b40d8a
Jeremy Meiss <jeremy.meiss@gmail.com>
2024-12-11 11:41:11 -0600

    add `showLastUpdateTime` on docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,6 +39,7 @@ const config = {
         docs: {
           routeBasePath: '/', // Serve the docs at the site's root
           sidebarPath: './sidebars.js',
+          showLastUpdateTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl:
